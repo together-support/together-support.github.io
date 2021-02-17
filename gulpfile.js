0362af5d1a0f00d6bff9300367dff526ce253f83
Xin Xin <9159424+xinemata@users.noreply.github.com>
2021-02-16 17:17:08 -0800

    updated puppeteer args for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -190,7 +190,7 @@ gulp.task('qunit', () => {
                 targetUrl: `http://${serverConfig.host}:${serverConfig.port}/${filename}`,
                 timeout: 20000,
                 redirectConsole: false,
-                puppeteerArgs: ['--allow-file-access-from-files']
+                puppeteerArgs: ['--no-sandbox', '--disable-setuid-sandbox', '--allow-file-access-from-files']
             })
                 .then(result => {
                     if( result.stats.failed > 0 ) {
