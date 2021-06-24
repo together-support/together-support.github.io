a418b3844fb338ce93e47f0313badb89eb0dda59
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2021-06-24 13:23:25 -0400

    adds colors, basic typc type

--- js/main.js
+++ js/main.js
@@ -1,37 +1,26 @@
 
-let scrollTop = 0;
+function isScrolledIntoView(element) {
+  var rect = element.getBoundingClientRect();
+  console.log(rect.top)
+  var html = document.documentElement;
+  return (
+    rect.top >= 0
+  );
+}
 
-document.getElementById("nav-trigger").addEventListener("click",function(){
-	if((window.innerHeight - 35) <= scrollTop){
-		if(document.getElementById("main-nav").classList.contains("open")){
-			document.getElementById("main-nav").classList.remove("open")
-			document.getElementById("main-nav").classList.add("closed")
-			document.getElementById("nav-trigger").setAttribute('aria-expanded', 'false');
-			document.getElementById("nav-status").innerHTML = "Closed"
-		}else {
-			document.getElementById("main-nav").classList.remove("closed")
-			document.getElementById("main-nav").classList.add("open")
-			document.getElementById("nav-trigger").setAttribute('aria-expanded', 'true');
-			document.getElementById("nav-status").innerHTML = "Open"
+window.onscroll = function() {whichSection()};
+
+
+function whichSection() {
+
+	let sections = document.getElementsByTagName('section');
+	for (var i = sections.length - 1; i >= 0; i--) {
+		console.log(isScrolledIntoView(sections[i]))
+		if(isScrolledIntoView(sections[i])){
+			if(document.querySelectorAll(".in-page-navigation a.on")[0]){
+				document.querySelectorAll(".in-page-navigation a.on")[0].classList.remove("on")
+			}
+			document.querySelectorAll(".in-page-navigation a[href='#"+sections[i].id+"']")[0].classList.add("on");
 		}
 	}
-	
-})
-
-window.onscroll = function() {
-    console.log('scrolling');
-    scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-   if( (window.innerHeight - 35) <= scrollTop){
-   	    if(!document.getElementById("main-nav").classList.contains("open") && !document.getElementById("main-nav").classList.contains("closed")){
-   	    	document.getElementById("main-nav").classList.add("closed")
-   	    	document.getElementById("nav-trigger").setAttribute('aria-expanded', 'false');
-   	    	document.getElementById("nav-status").innerHTML = "Closed"
-   	    }
-   		
-	} else {
-		document.getElementById("main-nav").classList.remove("closed");
-		document.getElementById("main-nav").classList.remove("open")
-		document.getElementById("nav-trigger").setAttribute('aria-expanded', 'true');
-		document.getElementById("nav-status").innerHTML = "Open"
-	}
-};
+}
